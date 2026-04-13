db45dffab00432216363718307febc82da1a42f3
retroBoy69 <oussamakraiem10@gmail.com>
2026-04-13 19:50:40 +0100

    Board model Implementation

---
+++ backend/models.py
@@ -0,0 +1,17 @@
+from sqlmodel import Field, SQLModel
+from typing import Optional
+
+# Stm32 board Model
+class Board(SQLModel, table = True):
+    id : Optional[int] = Field(default = None, primary_key = True)
+    name : str
+    serial_number : str = Field(unique = True)
+    config_file: Optional[str] = Field(default=None)
+    gdb_port : int
+    telnet_port : int
+    tcl_port : int
+    status : str = Field(default = "idle")
+    pi_host : str = Field(default = "localhost")
+    openocd_pid : Optional[int] = Field(default = None)
+
+
