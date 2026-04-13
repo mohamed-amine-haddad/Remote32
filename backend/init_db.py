cd9a5ebc7c21a29487ad46f9cbc4268588c98122
retroBoy69 <oussamakraiem10@gmail.com>
2026-04-13 11:35:27 +0100

    init database

---
+++ backend/init_db.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+conn = sqlite3.connect("remote32.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS boards (
+    id            INTEGER PRIMARY KEY AUTOINCREMENT,
+    name          TEXT    NOT NULL,
+    serial_number TEXT    NOT NULL UNIQUE,
+    config_file   TEXT    NOT NULL,
+    gdb_port      INTEGER NOT NULL UNIQUE,
+    telnet_port   INTEGER NOT NULL UNIQUE,
+    tcl_port      INTEGER NOT NULL UNIQUE,
+    status        TEXT    NOT NULL DEFAULT 'idle',
+    openocd_pid   INTEGER
+);""")
+
+
+conn.commit()
+conn.close()
