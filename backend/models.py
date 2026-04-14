9829a371f79e854397a164a4c436180ba8ebfdd2
retroBoy69 <oussamakraiem10@gmail.com>
2026-04-14 16:11:36 +0100

    Add unique constraint to ports and config_file

--- backend/models.py
+++ backend/models.py
@@ -6,10 +6,10 @@ class Board(SQLModel, table = True):
     id : Optional[int] = Field(default = None, primary_key = True)
     name : str
     serial_number : str = Field(unique = True)
-    config_file: Optional[str] = Field(default=None)
-    gdb_port : int
-    telnet_port : int
-    tcl_port : int
+    config_file: Optional[str] = Field(default=None, unique = True)
+    gdb_port : int = Field(unique = True)
+    telnet_port : int = Field(unique = True)
+    tcl_port : int = Field(unique = True)
     status : str = Field(default = "idle")
     pi_host : str = Field(default = "localhost")
     openocd_pid : Optional[int] = Field(default = None)
