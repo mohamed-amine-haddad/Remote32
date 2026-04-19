de998f15703c0941397e7c141f1cec5289123d30
retroBoy69 <oussamakraiem10@gmail.com>
2026-04-19 15:03:49 +0100

    _ssh and is_running methods implementation

--- backend/services/openocd.py
+++ backend/services/openocd.py
@@ -5,7 +5,8 @@ import paramiko
 from models import Board, RaspberryPi
 from sqlmodel import Session
 from database import engine
-from services.raspberrys import get_by_id
+from services.raspberrys import get_by_id as get_pi_by_id
+from services.devices import get_by_id as get_board_by_id
 
 session = paramiko.SSHClient()
 session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -16,10 +17,22 @@ def _ssh(board : Board):
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     with Session(engine) as session:
-          pi = get_by_id(session, board.pi_id)
+          pi = get_pi_by_id(session, board.pi_id)
 
     client.connect(hostname = pi.host, username = pi.user, password = pi.password)
     return client
+
+def is_running(board : Board):
+    openocd_pid = board.openocd_pid
+    if (openocd_pid == None):
+        return False
+    client = _ssh(board) 
+    stdin, stdout, stderr = client.exec_command(f"kill -0 {openocd_pid}") # checks process existence without affecting it
+    exit_code = stdout.channel.recv_exit_status()
+    client.close()
+    return exit_code == 0
+
+
 
 if __name__ == "__main__":
       """
@@ -28,8 +41,7 @@ if __name__ == "__main__":
         """
       with Session(engine) as session:
           board = session.get(Board, 1)
-      client = _ssh(board)
-      client.close()
+      print(is_running((board)))
 
 
 
