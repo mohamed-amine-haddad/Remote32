02e1a48ef83ae9670635730734b246b473e6f187
retroBoy69 <oussamakraiem10@gmail.com>
2026-04-19 13:25:05 +0100

    added raspberry pi class

--- backend/models.py
+++ backend/models.py
@@ -12,7 +12,7 @@ class Board(SQLModel, table = True):
     telnet_port : int = Field(unique = True)
     tcl_port : int = Field(unique = True)
     status : str = Field(default = "idle")
-    pi_host : str = Field(default = "localhost")
+    pi_id : int = Field(foreign_key = "raspberrypi.id")
     openocd_pid : Optional[int] = Field(default = None)
 
 
@@ -33,3 +33,9 @@ class ApplicationSession(SQLModel, table=True):
     start_time: datetime
     end_time: datetime
     status: str  # reserved | active | ended | cancelled
+
+class RaspberryPi(SQLModel, table=True):
+    id : Optional[int] = Field(default = None, primary_key = True)
+    host : str = Field(unique = True)
+    user : str
+    password : str
