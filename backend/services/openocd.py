880e423f9569f596541c436bf69208e56e4f3b3a
retroBoy69 <oussamakraiem10@gmail.com>
2026-04-21 11:42:58 +0100

    start_debugging_session() Implementation

--- backend/services/openocd.py
+++ backend/services/openocd.py
@@ -1,15 +1,18 @@
 import sys
 sys.path.insert(0, ".")
 
-import paramiko 
+import os
+import paramiko
+from dotenv import load_dotenv
+from fastapi import HTTPException
 from models import Board, RaspberryPi
 from sqlmodel import Session
 from database import engine
+
+load_dotenv()
 from services.raspberrys import get_by_id as get_pi_by_id
 from services.devices import get_by_id as get_board_by_id
-
-session = paramiko.SSHClient()
-session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+from services.devices import update as update_board
 
 # Opens and returns an SSH connection to the Pi that the board is connected to
 def _ssh(board : Board):
@@ -33,19 +36,36 @@ def is_running(board : Board):
 
     client.close()
     return process_name == "openocd"
-    
-    
 
+def start_debug_session(board_id : int) -> bool :
+    with Session(engine) as session:
+        board = get_board_by_id(session, board_id)
+
+    if is_running(board) :
+        raise HTTPException(status_code=409, detail="Board already has an active session")
+        
+    client = _ssh(board)
+    config_file_path = os.getenv("CONFIG_PATH") + board.config_file
+    
+    print ("path : ", config_file_path)
+
+    stdin, stdout, stderr = client.exec_command(f"nohup openocd -f {config_file_path} > /tmp/openocd_{board_id}.log 2>&1 & echo $!")
+
+    openocd_pid = stdout.readline().strip()
+    print("PID :", openocd_pid)
+
+    with Session(engine) as session:
+        update_board(session, board_id, {"status" : "running", "openocd_pid" : openocd_pid})
+       
+    print (f"updated status : running \nupdated pid : {openocd_pid}")
+    return board.gdb_port
 
 
 if __name__ == "__main__":
-      """
-      from database import engine
-      from sqlmodel import Session
-        """
       with Session(engine) as session:
           board = session.get(Board, 1)
-      print(is_running((board)))
+    
+      print(start_debug_session((1)))
 
 
 
